aeb1c5092a998b7cf4d5961d9ca57b3185c32946
Kacper Kozerski <kozerskikacper@gmail.com>
2025-03-25 18:46:08 +0100

    fixed a bug, where scenarios were used instead of scenario paths

--- src/objective/costs_under_risk.jl
+++ src/objective/costs_under_risk.jl
@@ -19,6 +19,11 @@
 
 
 scenario_probability(m::Model) = scen -> any_stochastic_scenario_weight(m; stochastic_scenario=scen)
+# TODO works only with trees
+path_probability(m::Model) = path -> any_stochastic_scenario_weight(m; stochastic_scenario=path[end])
+
+generate_path_costs(scenario_costs, stochastic_paths) = Dict(path => sum(get(scenario_costs, scen, 0) for scen in path) for path in stochastic_paths)
+
 
 """
     Scalarizes the stochastic goal function with expected value operator.
@@ -28,7 +33,9 @@ scenario_probability(m::Model) = scen -> any_stochastic_scenario_weight(m; stoch
     - scenario_dict - a dictionary with scenarios as keys and their respective cost functions as values
 """
 function costs_under_risk!(m::Model, scenario_costs::Dict, ::Val{:expected_value})
-    return expected_value(scenario_costs, scenario_probability(m))
+    #return expected_value(scenario_costs, scenario_probability(m))
+    path_costs = generate_path_costs(scenario_costs, m.ext[:spineopt].stochastic_structure[:full_stochastic_paths])
+    return expected_value(path_costs, path_probability(m))
 end
 
 """
@@ -40,7 +47,8 @@ end
 """
 function costs_under_risk!(m::Model, scenario_costs::Dict, ::Val{:cvar})
     beta = cvar_percentage(model=m.ext[:spineopt].instance)
-    return cvar(m, beta, scenario_costs, scenario_probability(m)) 
+    path_costs = generate_path_costs(scenario_costs, m.ext[:spineopt].stochastic_structure[:full_stochastic_paths])
+    return cvar(m, beta, path_costs, path_probability(m)) 
 end
 
 """
@@ -53,7 +61,8 @@ end
 function costs_under_risk!(m::Model, scenario_costs::Dict, ::Val{:markowitz})
     lambda = dispersion_to_mean_cost_weight(model=m.ext[:spineopt].instance)
     dispersion_type = Val(dispersion_measure(model=m.ext[:spineopt].instance))
-    return markowitz_model(m::Model, lambda, scenario_costs, scenario_probability(m), dispersion_type)
+    path_costs = generate_path_costs(scenario_costs, m.ext[:spineopt].stochastic_structure[:full_stochastic_paths])
+    return markowitz_model(m::Model, lambda, path_costs, path_probability(m), dispersion_type)
 end
 
 """
@@ -66,36 +75,35 @@ end
 expected_value(scenario_costs, probability::Function) = sum(cost * probability(scen) for (scen, cost) in scenario_costs; init=0)
 
 """
-    Returns CVAR LP formulation for given goal functions per scenarios and their appropriate probabilities.
+    Returns CVAR LP formulation for given goal functions per scenario paths and their appropriate probabilities.
     It results in optimization of beta worst cases (highest costs).
 
     # Arguments:
     - m - JuMP model
     - beta - percentage of worst cases optimized by the CVAR
-    - scenario_costs - a dictionary with scenarios as keys and their respective cost functions as values
-    - scenario_probability - a function that for evey scenario returns its probability 
+    - path_costs - a dictionary with paths as keys and their respective cost functions as values
+    - path_probability - a function that for evey path returns its probability 
 """
-function cvar(m::Model, beta::Float64, scenario_costs::Dict, scenario_probability::Function)
+function cvar(m::Model, beta::Float64, path_costs::Dict, path_probability::Function)
     !(0 < beta <= 1) && throw(DomainError(beta, "parameter not in the domain 0 < beta <= 1"))
     @variable(m, v)
-    p = scenario_probability
-    return v + 1/beta * sum(p(scen) * positive_part_of_lp_term(m, cost - v) for (scen, cost) in scenario_costs; init=0)
+    return v + 1/beta * sum(path_probability(scen) * positive_part_of_lp_term(m, cost - v) for (scen, cost) in path_costs; init=0)
 end
 
 """
-    Returns Markowitz LP formulation for given goal functions per scenarios and their appropriate probabilities.
+    Returns Markowitz LP formulation for given goal functions per scenario paths and their appropriate probabilities.
     It is a scalarization of problem of both cost and dispersion minimization.
 
     # Arguments:
     - m - JuMP model
     - lambda - factor of tradeoff between dispersion and expected value, the lower the more attention is given to expectd value
-    - scenario_costs - a dictionary with scenarios as keys and their respective cost functions as values
-    - scenario_probability - a function that for evey scenario returns its probability 
+    - path_costs - a dictionary with paths as keys and their respective cost functions as values
+    - path_probability - a function that for evey scenario returns its probability 
 """
-function markowitz_model(m::Model, lambda::Float64, scenario_costs::Dict, scenario_probability::Function, dispersion_type::Val)
+function markowitz_model(m::Model, lambda::Float64, path_costs::Dict, path_probability::Function, dispersion_type::Val)
     !(0 < lambda < 1) && throw(DomainError(lambda, "parameter not in the domain 0 < lambda < 1"))
-    mu = expected_value(scenario_costs, scenario_probability)
-    delta = dispersion_metric(m, scenario_costs, scenario_probability, dispersion_type)
+    mu = expected_value(path_costs, path_probability)
+    delta = dispersion_metric(m, path_costs, path_probability, dispersion_type)
     return mu + lambda * delta
 end
 
@@ -130,13 +138,13 @@ semideviation(m::Model, cost, mu) = positive_part_of_lp_term(m, cost - mu)
     
     # Arguments:
     - m - JuMP model
-    - scenario_costs - a dictionary with scenarios as keys and their respective cost functions as values
-    - probability - a function that for evey scenario returns its probability 
+    - path_costs - a dictionary with scenario paths as keys and their respective cost functions as values
+    - probability - a function that for every scenario path returns its probability 
 """
-function dispersion_metric(m::Model, scenario_costs::Dict, probability::Function, ::Val{:max_semideviation})
-    mu = expected_value(scenario_costs, probability)
+function dispersion_metric(m::Model, path_costs::Dict, probability::Function, ::Val{:max_semideviation})
+    mu = expected_value(path_costs, probability)
     d = @variable(m, lower_bound=0)
-    for (scen, cost) in scenario_costs
+    for (scen, cost) in path_costs
         @constraint(m, d >= semideviation(m, cost, mu))
     end
     return d
@@ -148,28 +156,28 @@ end
     
     # Arguments:
     - m - JuMP model
-    - scenario_costs - a dictionary with scenarios as keys and their respective cost functions as values
-    - probability - a function that for evey scenario returns its probability 
+    - path_costs - a dictionary with paths as keys and their respective cost functions as values
+    - probability - a function that for every path returns its probability 
 """
-function dispersion_metric(m::Model, scenario_costs::Dict, probability::Function, ::Val{:avg_semideviation})
-    mu = expected_value(scenario_costs, probability)
-    scenario_semideviations = Dict(scen => semideviation(m, cost, mu) for (scen, cost) in scenario_costs)
+function dispersion_metric(m::Model, path_costs::Dict, probability::Function, ::Val{:avg_semideviation})
+    mu = expected_value(path_costs, probability)
+    scenario_semideviations = Dict(scen => semideviation(m, cost, mu) for (scen, cost) in path_costs)
     return expected_value(scenario_semideviations, probability)
 end
 
 """
     Returns an LP formulation for average Gini difference.
-    We want to minimize the average differences between scenario cost, thus to achieve as clustered results as possible.
+    We want to minimize the average differences between scenario path cost, thus to achieve as clustered results as possible.
     
     # Arguments:
     - m - JuMP model
-    - scenario_costs - a dictionary with scenarios as keys and their respective cost functions as values
-    - probability - a function that for evey scenario returns its probability 
+    - path_costs - a dictionary with scenario paths as keys and their respective cost functions as values
+    - probability - a function that for every scenario oath returns its probability 
 """
-function dispersion_metric(m::Model, scenario_costs::Dict, probability::Function, ::Val{:avg_gini_difference})
+function dispersion_metric(m::Model, path_costs::Dict, probability::Function, ::Val{:avg_gini_difference})
     return sum(
         positive_part_of_lp_term(m, cost1 - cost2) * probability(scen1) * probability(scen2)
-        for (scen1, cost1) in scenario_costs for (scen2, cost2) in scenario_costs if scen1 != scen2
+        for (scen1, cost1) in path_costs for (scen2, cost2) in path_costs if scen1 != scen2
         ; init=0
     )
 end
