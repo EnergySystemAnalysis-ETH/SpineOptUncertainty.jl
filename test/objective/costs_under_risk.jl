4fa0cdd2ee06f2165383e2a471e9c07bcbd66252
Kacper Kozerski <kozerskikacper@gmail.com>
2025-03-28 15:05:32 +0100

    added tests for calling the risk approaches from run_spineopt

--- test/objective/costs_under_risk.jl
+++ test/objective/costs_under_risk.jl
@@ -590,6 +590,184 @@ function test_markowitz_optimization()
         end
     end
 end
+
+function setup_risk_approach_case(risk_approach="expected_value"; cvar_percentage=0.5, dispersion_measure="max_semideviation", lambda=0.5)
+    url_in = "sqlite://"
+    file_path_out = "$(@__DIR__)/test_out.sqlite"
+    url_out = "sqlite:///$file_path_out"
+    objects = [
+        ["model", "simple"],
+        ["report", "report1"],
+        ["stochastic_scenario", "parent"],
+        ["stochastic_scenario", "child1"],
+        ["stochastic_scenario", "child2"],
+        ["stochastic_structure", "DAG"],
+        ["temporal_block", "investment"],
+        ["temporal_block", "operation2030"],
+        ["report", "report1"],
+        ["node", "fuel_node"],
+        ["node", "electricity_node"],
+        ["unit", "safe_plant"],
+        ["unit", "risky_plant"]
+    ]
+    relationships = [
+        ["model__default_investment_stochastic_structure", ["simple", "DAG"]],
+        ["model__default_investment_temporal_block", ["simple", "investment"]],
+        ["model__default_stochastic_structure", ["simple", "DAG"]],
+        ["model__default_temporal_block", ["simple", "operation2030"]],
+        ["model__report", ["simple", "report1"]],
+        ["model__stochastic_structure", ["simple", "DAG"]],
+        ["model__temporal_block", ["simple", "investment"]],
+        ["model__temporal_block", ["simple", "operation2030"]],
+        ["stochastic_structure__stochastic_scenario", ["DAG", "parent"]],
+        ["stochastic_structure__stochastic_scenario", ["DAG", "child1"]],
+        ["stochastic_structure__stochastic_scenario", ["DAG", "child2"]],
+        ["report__output", ["report1", "unit_flow"]],
+        ["unit__from_node", ["safe_plant", "fuel_node"]],
+        ["unit__from_node", ["risky_plant", "fuel_node"]],
+        ["unit__to_node", ["safe_plant", "electricity_node"]],
+        ["unit__to_node", ["risky_plant", "electricity_node"]],
+        ["unit__node__node", ["safe_plant", "electricity_node", "fuel_node"]],
+        ["unit__node__node", ["risky_plant", "electricity_node", "fuel_node"]],
+        ["parent_stochastic_scenario__child_stochastic_scenario", ["parent", "child1"]],
+        ["parent_stochastic_scenario__child_stochastic_scenario", ["parent", "child2"]],
+    ]
+    object_parameter_values = [
+        ["model", "simple", "risk_approach", risk_approach],
+        ["model", "simple", "cvar_percentage", cvar_percentage],
+        ["model", "simple", "dispersion_measure", dispersion_measure],
+        ["model", "simple", "dispersion_to_mean_cost_weight", lambda],
+        ["model", "simple", "model_end", Dict("data" => "2031-01-01T00:00:00.0", "type" => "date_time")],
+        ["model", "simple", "model_start", Dict("data" => "2030-01-01T00:00:00.0", "type" => "date_time")],
+        ["node", "fuel", "balance_type", "balance_type_none"],
+        ["node", "electricity_node", "demand", 100.0],
+        ["node", "fuel_node", "balance_type","balance_type_none"],
+        ["temporal_block", "investment", "resolution", Dict("data"=> "6M", "type"=> "duration")],
+        ["temporal_block", "operation2030", "block_end", Dict( "data" => "2031-01-01T00:00:00.0", "type"=> "date_time")],
+        ["temporal_block", "operation2030", "block_start", Dict("data"=> "2030-01-01T00:00:00.0", "type"=> "date_time")],
+        ["temporal_block", "operation2030", "resolution", Dict("data"=> "6M", "type"=> "duration")],
+        ["unit", "safe_plant", "candidate_units", Dict("data"=> Dict(
+                    "2030-01-01T00:00:00.0"=> 1.0,
+                    "2030-07-01T00:00:00.0"=> 0.0
+                ),
+                "type"=> "time_series"
+            )
+        ],
+        ["unit", "safe_plant", "initial_units_invested_available", 0],
+        ["unit", "safe_plant", "number_of_units", 0],
+        ["unit", "safe_plant", "unit_investment_cost", 1e9],
+        ["unit", "safe_plant", "unit_investment_variable_type", "unit_investment_variable_type_integer"],
+        ["unit","safe_plant","units_on_cost",0],
+        ["unit", "risky_plant", "candidate_units", Dict("data"=> Dict(
+                    "2030-01-01T00:00:00.0"=> 1.0,
+                    "2030-07-01T00:00:00.0"=> 0.0
+                ),
+                "type"=> "time_series"
+            )
+        ],
+        ["unit", "risky_plant", "initial_units_invested_available", 0],
+        ["unit", "risky_plant", "number_of_units", 0],
+        ["unit", "risky_plant", "unit_investment_cost", 1e9],
+        ["unit", "risky_plant", "unit_investment_variable_type", "unit_investment_variable_type_integer"],
+        ["unit","risky_plant","units_on_cost",0],
+    ]
+    relationship_parameter_values = [
+        [
+        "unit__from_node", ["safe_plant", "fuel_node"], "vom_cost", Dict(
+            "data" => [["parent", 52], ["child1", 52], ["child2", 52]],
+            "type" => "map",
+            "index_type" => "str"
+        ),],
+        [
+            "unit__from_node", ["risky_plant", "fuel_node"], "vom_cost",
+            Dict(
+                "data" => [["parent", 50], ["child1", 30], ["child2", 70]],
+                "type" => "map",
+                "index_type" => "str"
+            ),
+            ],
+        ["unit__node__node", ["safe_plant", "electricity_node", "fuel_node"], "fix_ratio_out_in_unit_flow", 0.7],
+        ["unit__to_node", ["safe_plant", "electricity_node"], "unit_capacity", 100],
+        ["unit__node__node", ["risky_plant", "electricity_node", "fuel_node"], "fix_ratio_out_in_unit_flow", 0.7],
+        ["unit__to_node", ["risky_plant", "electricity_node"], "unit_capacity", 100],
+        ["stochastic_structure__stochastic_scenario", ["DAG", "child1"], "weight_relative_to_parents", 0.5],
+        ["stochastic_structure__stochastic_scenario", ["DAG", "child2"], "weight_relative_to_parents", 0.5],
+        ["stochastic_structure__stochastic_scenario", ["DAG", "parent"], "stochastic_scenario_end", Dict("type" => "duration", "data" => "6M")],
+    
+    
+    ]
+    test_data = Dict(
+        :objects => objects,
+        :relationships => relationships,
+        :object_parameter_values =>  object_parameter_values,
+        :relationship_parameter_values => relationship_parameter_values
+    )
+    _load_test_data(url_in, test_data)
+    url_in, url_out, file_path_out
+end
+
+function should_be_invested(m::Model, to_invest::Symbol)
+    @fetch units_invested = m.ext[:spineopt].variables
+    for (k,v) in units_invested
+        if (
+            k.unit == Object(to_invest, :unit) &&
+            k.stochastic_scenario == Object(:parent, :stochastic_scenario) &&
+            k.t.start[] == DateTime(2030,1,1) &&
+            k.t.end_[] == DateTime(2030,7,1)
+        )
+            @test isapprox(value(v), 1)
+        else
+            @test isapprox(value(v), 0) 
+        end
+    end
+end
+function test_expected_value_call()
+    @testset "expected value call" begin
+        url_in, url_out, file_path_out = setup_risk_approach_case("expected_value")
+        m = run_spineopt(url_in, url_out; log_level=1)
+        should_be_invested(m, :risky_plant)
+    end
+end
+
+function test_cvar_call()
+    @testset "cvar call" begin
+        @testset "cvar risk averse" begin
+            url_in, url_out, file_path_out = setup_risk_approach_case("cvar"; cvar_percentage=0.01)
+            m = run_spineopt(url_in, url_out; log_level=1)
+            should_be_invested(m, :safe_plant)
+        end
+        @testset "cvar less risk averse" begin
+            url_in, url_out, file_path_out = setup_risk_approach_case("cvar"; cvar_percentage=1.)
+            m = run_spineopt(url_in, url_out; log_level=1)
+            should_be_invested(m, :risky_plant)
+        end
+
+    end
+end
+
+function test_markowitz_dispersion_call(dispersion_measure)
+    @testset "$dispersion_measure" begin
+        @testset "more risk averse" begin
+            url_in, url_out, file_path_out = setup_risk_approach_case("markowitz"; dispersion_measure=dispersion_measure, lambda=0.99)
+            m = run_spineopt(url_in, url_out; log_level=1)
+            should_be_invested(m, :safe_plant)
+        end
+        @testset "less risk averse" begin
+            url_in, url_out, file_path_out = setup_risk_approach_case("markowitz"; dispersion_measure=dispersion_measure, lambda=0.01)
+            m = run_spineopt(url_in, url_out; log_level=1)
+            should_be_invested(m, :risky_plant)
+        end
+    end
+end
+
+function test_markowitz_call()
+    @testset "markowitz call" begin
+        test_markowitz_dispersion_call("max_semideviation")
+        test_markowitz_dispersion_call("avg_semideviation")
+        test_markowitz_dispersion_call("avg_gini_difference")
+    end
+end
+
 @testset "costs under risk" begin
     test_expected_value()
     test_positive_part()
@@ -598,4 +776,7 @@ end
     test_expected_value_optimization()
     test_cvar_optimization()
     test_markowitz_optimization()
+    test_expected_value_call()
+    test_cvar_call()
+    test_markowitz_call()
 end
